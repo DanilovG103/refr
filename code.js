6ebeb8b943b14f85cc88ff436d67918620706c8c
DanilovG103 <80414793+DanilovG103@users.noreply.github.com>
2021-03-22 17:57:49 +0400

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,18 @@
-function func(s, a, b) {
-    if (s.match(/^$/)) return -1;
+function refactored(s, a, b) {
+  if (!s) return -1;
 
-    let i = s.length - 1;
-    let aIndex = -1;
-    let bIndex = -1;
-    
-    while ((aIndex == -1) && (bIndex == -1) && (i > 0)){
-        if (s.slice(i) == a) aIndex = i;
-        if (s.slice(i) == b) bIndex = i;
-        i--;
-    }
+  let i = s.length - 1;
+  let aIndex = -1;
+  let bIndex = -1;
 
-    if (aIndex != -1){
-        if (bIndex == -1) return aIndex;
-        else return Math.max(aIndex,bIndex);
-    }else{
-        if (bIndex != -1) return bIndex;
-        else return -1;
-    }
+  while (aIndex === -1 && bIndex === -1 && i > 0) {
+    if (s.slice(i,i+1) === a) aIndex = i;
+    if (s.slice(i,i+1) === b) bIndex = i;
+    i--;
+  }
+
+  if (aIndex !== -1 || bIndex === -1) return Math.max(aIndex, bIndex);
+  
+  if (bIndex) return bIndex;
+  else return -1;
 }
