a9222d9224d0708dfa03997be5fb1278808acf95
DanilovG103 <80414793+DanilovG103@users.noreply.github.com>
2021-03-23 19:42:01 +0400

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,16 @@
 function refactored(s, a, b) {
-  if (!s) return -1;
-
   let i = s.length - 1;
   let aIndex = -1;
   let bIndex = -1;
 
-  while (aIndex === -1 && bIndex === -1 && i > 0) {
-    if (s.slice(i,i+1) === a) aIndex = i;
-    if (s.slice(i,i+1) === b) bIndex = i;
+  while (aIndex === -1 && bIndex === -1 && i >= 0) {
+    if (s.slice(i, i + 1) === a) aIndex = i;
+    if (s.slice(i, i + 1) === b) bIndex = i;
     i--;
   }
 
   if (aIndex !== -1 || bIndex === -1) return Math.max(aIndex, bIndex);
-  
+
   if (bIndex) return bIndex;
   else return -1;
 }
